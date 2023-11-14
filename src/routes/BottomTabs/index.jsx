e121deac319ff45192998ddc1179c9530bcc9ac6
Jefferson Moraes <jeffersonjuniormoraes@gmail.com>
2023-11-14 10:40:37 -0300

    colocando icons navegation

--- src/routes/BottomTabs/index.jsx
+++ src/routes/BottomTabs/index.jsx
@@ -1,16 +1,40 @@
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { Image } from 'react-native';
 import { Home } from '../../screens/Home/index';
 import Cupons from '../../screens/Cupons/index';
+import logo from '../../assets/images/logo.png';
+import cupom from '../../assets/images/icon_cupom.png';
 
 const Tab = createBottomTabNavigator();
 
 function BottomTabs() {
    return (
-      <Tab.Navigator screenOptions={{
-         headerShown: false,
-      }}>
-         <Tab.Screen name="Home" component={Home} />
-         <Tab.Screen name="Cupons" component={Cupons} />
+      <Tab.Navigator
+         screenOptions={{
+            headerShown: false
+         }}>
+         <Tab.Screen
+            options={{
+               tabBarIcon: ({ color }) => (
+                  <Image
+                     resizeMode="contain"
+                     source={logo}
+                     style={{ tintColor: color, width: 25 }}
+                  />
+               )
+            }}
+            name="Home"
+            component={Home}
+         />
+         <Tab.Screen options={{
+               tabBarIcon: ({ color }) => (
+                  <Image
+                     resizeMode="contain"
+                     source={cupom}
+                     style={{ tintColor: color, width: 25 }}
+                  />
+               )
+            }} name="Cupons" component={Cupons} />
       </Tab.Navigator>
    );
 }
